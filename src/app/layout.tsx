17389dacde0057b6fd8c5a48e7e9957f32f43eaf
leonyllukejayme <leonylljayme@gmail.com>
2024-06-26 22:12:31 +0800

    fixed navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout({
     <ClerkProvider>
         <html lang="en">
         <body className={inter.className}>
-          <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 fixed z-10 top-0 right-0 left-0">
               <Navbar />
           </div>
-          <div className="w-full bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          <div className="w-full mt-24 bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
             {children}
           </div>
           </body>
