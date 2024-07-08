d866097ee31508854c47ee11faa6dbe255acbf54
leonyllukejayme <leonylljayme@gmail.com>
2024-07-08 14:35:30 +0800

    Day 2

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,21 @@
+import Feed from "@/components/Feed"
+import LeftMenu from "@/components/LeftMenu"
+import RightMenu from "@/components/RightMenu"
+
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='flex gap-6 pt-6'>
+    {/* LEFT */}
+    <div className="hidden xl:block w-[20%]"><LeftMenu/></div>
+    {/* CENTER */}
+    <div className="w-full lg:w-[70%] xl:w-[50%]">
+      <div className="flex flex-col gap-6">
+        <Feed />
+      </div>
+    </div>
+    {/* RIGHT */}
+    <div className="hidden lg:block w-[30%]"><RightMenu userId="12345677567547"/></div>
+  </div>
   )
 }
 
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,9 @@
+const ProfileCard = () => {
+  return (
+    <div className=''>
+      ProfileCard
+    </div>
+  )
+};
+
+export default ProfileCard;
