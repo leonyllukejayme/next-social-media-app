089f06804f19089eaad046d8ffe4eb20d7e917bc
leonyllukejayme <leonylljayme@gmail.com>
2024-08-15 09:45:12 +0800

    img.clerk.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    reactCompiler: true,
-    ppr: 'incremental'
-  },
-  images:{
-    remotePatterns:[
-      {
-        protocol:'https',
-        hostname:'upload.wikimedia.org'
-      },
-      {
-        protocol:'https',
-        hostname:'img.freepik.com'
-      }
-    ]
-  }
+	experimental: {
+		reactCompiler: true,
+		ppr: 'incremental',
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'upload.wikimedia.org',
+			},
+			{
+				protocol: 'https',
+				hostname: 'img.freepik.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'img.clerk.com',
+			},
+		],
+	},
 };
 
 export default nextConfig;
